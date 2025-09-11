924c995555cb9a29805886f01aea1b72ac674f8d
abretz-mimacom <133235099+abretz-mimacom@users.noreply.github.com>
2025-09-11 21:52:23 +0000

    update scripts

--- scripts/create-ns-secrets.sh
+++ scripts/create-ns-secrets.sh
@@ -4,12 +4,49 @@ set -o errexit
 
 #!/bin/bash
 
+# Check for required argument (namespace)
 if [ -z "$1" ]; then
   echo "must specify namespace for secret creation"
   exit 1
 else
-  echo "using namespace: $1"
-  DEPLOYMENT_NAMESPACE=$1
+  echo "creating secrets for namespace: $1"
+  DEPLOYMENT_NAMESPACE="$1"
+fi
+
+
+if kubectl get namespace "$DEPLOYMENT_NAMESPACE" >/dev/null 2>&1; then
+  echo "Namespace $DEPLOYMENT_NAMESPACE exists. Will not attempt to create it."
+else
+  echo "Namespace $DEPLOYMENT_NAMESPACE does not exist. Creating it now."
+  source "$CODESPACE_VSCODE_FOLDER/scripts/create-ns.sh $DEPLOYMENT_NAMESPACE"
+fi
+
+# Check for FLOWABLE_LICENSE_KEY
+if [ -z "$FLOWABLE_LICENSE_KEY" ]; then
+  echo "FLOWABLE_LICENSE_KEY environment variable is not set. Your deployment is likely to fail."
+else
+  echo "FLOWABLE_LICENSE_KEY is set. Writing its contents to $HOME/.flowable/flowable.license"
+  mkdir -p "$HOME/.flowable"
+  echo "$FLOWABLE_LICENSE_KEY" > "$HOME/.flowable/flowable.license"
+  chmod 600 "$HOME/.flowable/flowable.license"
+fi
+
+if [ -z "$2" ]; then
+  echo "no license file path specified, using '$HOME/.flowable/flowable.license' by default"
+else
+  echo "using license file path: $2"
+fi
+
+LICENSE_FILE_PATH="${2:-$HOME/.flowable/flowable.license}"
+
+if [ -z "$FLOWABLE_REPO_USER" ]; then
+  echo "must have FLOWABLE_REPO_USER env variable set for secret creation"
+  exit 1
+fi
+
+if [ -z "$FLOWABLE_REPO_PASSWORD" ]; then
+  echo "must have FLOWABLE_REPO_PASSWORD env variable set for secret creation"
+  exit 1
 fi
 
 kubectl create secret docker-registry yourReleaseName-flowable-regcred \
@@ -18,4 +55,6 @@ kubectl create secret docker-registry yourReleaseName-flowable-regcred \
   --docker-password="$FLOWABLE_REPO_PASSWORD"> \
   --namespace $DEPLOYMENT_NAMESPACE
 
-kubectl create secret generic yourReleaseName-flowable-license --from-file=flowable.license=/your/local/licensefile/location/flowable.license --namespace $DEPLOYMENT_NAMESPACE
+kubectl create secret generic $RELEASE_NAME-flowable-license \
+  --from-file=flowable.license="$LICENSE_FILE_PATH" \
+  --namespace $DEPLOYMENT_NAMESPACE
