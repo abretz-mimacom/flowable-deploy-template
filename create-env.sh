216faf7c63c88fea0aafd66f677175787ca3ccdb
abretz-mimacom <133235099+abretz-mimacom@users.noreply.github.com>
2025-09-12 14:53:16 +0000

    update create script

--- create-env.sh
+++ create-env.sh
@@ -3,11 +3,15 @@
 NAMESPACE="${1:-dev}"
 RELEASE_NAME="${2:-flowable}"
 
+# Get latest scripts
 git submodule update --init --recursive --remote --force
 chmod a+x scripts/*
 
+# Setup kind cluster
 bash -c "$CODESPACE_VSCODE_FOLDER/scripts/kind-cluster-setup.sh" "$NAMESPACE"
 
+# Deploy Flowable Platform
 "$CODESPACE_VSCODE_FOLDER/scripts/deploy-flowable-platform.sh" "$NAMESPACE" "$RELEASE_NAME"
+
 
 k9s -n "$NAMESPACE"
