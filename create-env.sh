a0ffadb37ffa8a52af7dfe771c42ff6c76999542
abretz-mimacom <133235099+abretz-mimacom@users.noreply.github.com>
2025-09-12 15:14:57 +0000

    add cluster name

--- create-env.sh
+++ create-env.sh
@@ -2,13 +2,15 @@
 
 NAMESPACE="${1:-dev}"
 RELEASE_NAME="${2:-flowable}"
+CLUSTER_NAME="${3:-kind}"
 
 # Get latest scripts
 git submodule update --init --recursive --remote --force
 chmod a+x scripts/*
 
 # Setup kind cluster
-bash -c "$CODESPACE_VSCODE_FOLDER/scripts/kind-cluster-setup.sh" "$NAMESPACE"
+echo "Setting up kind cluster '$CLUSTER_NAME' and deploying Flowable Platform in namespace '$NAMESPACE' with release name '$RELEASE_NAME'"
+bash -c "$CODESPACE_VSCODE_FOLDER/scripts/kind-cluster-setup.sh $CLUSTER_NAME" 
 
 # Deploy Flowable Platform
 "$CODESPACE_VSCODE_FOLDER/scripts/deploy-flowable-platform.sh" "$NAMESPACE" "$RELEASE_NAME"
