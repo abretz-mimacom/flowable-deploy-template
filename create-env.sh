1e424ae663e37fdeb5a0a72e9c81cac172a28278
abretz-mimacom <133235099+abretz-mimacom@users.noreply.github.com>
2025-09-16 17:34:22 +0000

    update create-env

--- create-env.sh
+++ create-env.sh
@@ -15,8 +15,4 @@ bash -c "$CODESPACE_VSCODE_FOLDER/scripts/kind-cluster-setup.sh $CLUSTER_NAME"
 # Deploy Flowable Platform
 "$CODESPACE_VSCODE_FOLDER/scripts/deploy-flowable-platform.sh" "$NAMESPACE" "$RELEASE_NAME"
 
-# Add action runner
-"$CODESPACE_VSCODE_FOLDER/scripts/add-github-action-runner.sh" "CLUSTER_NAME"
-
-
 k9s -n "$NAMESPACE"
