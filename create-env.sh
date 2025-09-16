de7a9d436efa9bc24f18113624254cd49e13236a
abretz-mimacom <133235099+abretz-mimacom@users.noreply.github.com>
2025-09-16 18:49:07 +0000

    create/delete entire env

--- create-env.sh
+++ create-env.sh
@@ -1,18 +1,42 @@
+
 #!/bin/bash
 
-NAMESPACE="${1:-dev}"
-RELEASE_NAME="${2:-flowable}"
-CLUSTER_NAME="${3:-kind}"
+# Reusable function for cluster setup
+setup_cluster() {
+	local cluster_name="$1"
+	echo "Setting up kind cluster '$cluster_name'"
+	bash -c "$CODESPACE_VSCODE_FOLDER/scripts/kind-cluster-setup.sh $cluster_name"
+}
+
+# Reusable function for deployment
+deploy_flowable() {
+	local namespace="$1"
+	local release_name="$2"
+	echo "Deploying Flowable Platform in namespace '$namespace' with release name '$release_name'"
+	"$CODESPACE_VSCODE_FOLDER/scripts/deploy-flowable-platform.sh" "$namespace" "$release_name"
+}
 
 # Get latest scripts
 git submodule update --init --recursive --remote --force
 chmod a+x scripts/*
 
-# Setup kind cluster
-echo "Setting up kind cluster '$CLUSTER_NAME' and deploying Flowable Platform in namespace '$NAMESPACE' with release name '$RELEASE_NAME'"
-bash -c "$CODESPACE_VSCODE_FOLDER/scripts/kind-cluster-setup.sh $CLUSTER_NAME" 
-
-# Deploy Flowable Platform
-"$CODESPACE_VSCODE_FOLDER/scripts/deploy-flowable-platform.sh" "$NAMESPACE" "$RELEASE_NAME"
-
-k9s -n "$NAMESPACE"
+# Check for --all flag
+if [[ "$1" == "--all" ]]; then
+	# Array of configurations: (namespace release_name cluster_name)
+	configs=(
+		"dev flowable qa"
+        "test flowable qa"
+		"stg flowable prod"
+	)
+	for config in "${configs[@]}"; do
+		set -- $config
+		setup_cluster "$3"
+		deploy_flowable "$1" "$2"
+	done
+else
+	NAMESPACE="${1:-dev}"
+	RELEASE_NAME="${2:-flowable}"
+	CLUSTER_NAME="${3:-kind}"
+	setup_cluster "$CLUSTER_NAME"
+	deploy_flowable "$NAMESPACE" "$RELEASE_NAME"
+fi
