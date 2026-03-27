d29d3b64a2e746399a9a5a9b5c56e4ea65fc1df9
abretz-mimacom <133235099+abretz-mimacom@users.noreply.github.com>
2026-03-27 17:05:01 +0000

    use env var for setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git remote set-url origin https://github.com/abretz-mimacom/flowable-deploy-template.git
+git remote set-url origin "https://github.com/${GITHUB_REPOSITORY}.git"
 
 git submodule sync --recursive && git submodule update --init --recursive
