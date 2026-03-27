77ed7216cb1f3e3597b564386e26bc778aa02beb
abretz-mimacom <133235099+abretz-mimacom@users.noreply.github.com>
2026-03-27 17:03:31 +0000

    changes

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git remote set-url origin git@github.com:"${GITHUB_REPOSITORY}".git
+git remote set-url origin https://github.com/abretz-mimacom/flowable-deploy-template.git
 
 git submodule sync --recursive && git submodule update --init --recursive
