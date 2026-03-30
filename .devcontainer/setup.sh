31074e4cc2d33a1d25b29ef74fb6868ec7da2918
abretz-mimacom <133235099+abretz-mimacom@users.noreply.github.com>
2026-03-30 12:12:34 +0000

    alter setup scripts

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-git remote add origin "https://github.com/${GITHUB_REPOSITORY}.git"
+git remote add origin x
+git fetch
+git checkout dev
 
 git submodule sync --recursive && git submodule update --init --recursive
 
 chmod +x scripts/*
 
-## set redirect and weborigin uris for keycloak container
-jq --arg uri "https://${CODESPACE_NAME}-443.app.github.dev/login/*" '.clients[] |= if .clientId == "global-sales-demo" then .redirectUris[0] = $uri else . end' docker/keycloak/global-sales-demo-realm.json > /tmp/global-sales-demo-realm.json
-mv /tmp/global-sales-demo-realm.json docker/keycloak/global-sales-demo-realm.json	
-
-jq --arg uri "https://${CODESPACE_NAME}-443.app.github.dev/*" '.clients[] |= if .clientId == "global-sales-demo" then .webOrigins[0] = $uri else . end' docker/keycloak/global-sales-demo-realm.json > /tmp/global-sales-demo-realm.json
-mv /tmp/global-sales-demo-realm.json docker/keycloak/global-sales-demo-realm.json
-
-export KEYCLOAK_CLIENT_SECRET=some-super-secret-key
-jq --arg secret "${KEYCLOAK_CLIENT_SECRET}" '.clients[] |= if .clientId == "global-sales-demo" then .secret = $secret else . end' docker/keycloak/global-sales-demo-realm.json > /tmp/global-sales-demo-realm.json
-mv /tmp/global-sales-demo-realm.json docker/keycloak/global-sales-demo-realm.json
-
-jq --arg uri "https://${CODESPACE_NAME}-443.app.github.dev/login" '.clients[] |= if .clientId == "global-sales-demo" then .adminUrl = $uri else . end' docker/keycloak/global-sales-demo-realm.json > /tmp/global-sales-demo-realm.json
-mv /tmp/global-sales-demo-realm.json docker/keycloak/global-sales-demo-realm.json
