202d0c1b04061193c2ef4154f384711c8ea72fe9
abretz-mimacom <133235099+abretz-mimacom@users.noreply.github.com>
2026-03-31 17:09:51 -0500

    use github oauth (#40)
    * use github oauth
    
    * enable control/elasticsearch stg
    
    ---------
    
    Co-authored-by: abretz47 <abretz47@gmail.com>

--- create-env.sh
+++ create-env.sh
@@ -2,39 +2,24 @@
 #!/bin/bash
 
 DISABLE_ARC="${4:-false}"
-SINGLE_NODE="${5:-false}"
+
 # Reusable function for cluster setup
 setup_cluster() {
 	local cluster_name="$1"
 	echo "Setting up kind cluster '$cluster_name'"
 	export EXTRA_MOUNT_HOST_PATH=docker/keycloak
-	"$CODESPACE_VSCODE_FOLDER/scripts/kind-cluster-setup.sh" "$cluster_name" false false
+	"$CODESPACE_VSCODE_FOLDER/scripts/kind-cluster-setup.sh" "$cluster_name" $DISABLE_ARC
 	bash -c "echo \"Opening new shell\""
 
-	export KEYCLOAK_BASE_URL="${CODESPACE_NAME}-80.app.github.dev"
-	# export LOGIN_URL="${KEYCLOAK_BASE_URL}/login"
-	## set redirect and weborigin uris for keycloak container
-	jq --arg uri "https://${KEYCLOAK_BASE_URL}/*" '.clients[] |= if .clientId == "global-sales-demo" then .redirectUris[0] = $uri else . end' docker/keycloak/global-sales-demo-realm.json > /tmp/global-sales-demo-realm.json
-	mv /tmp/global-sales-demo-realm.json docker/keycloak/realm.json	
-	
-	jq --arg uri "https://${KEYCLOAK_BASE_URL}/*" '.clients[] |= if .clientId == "global-sales-demo" then .webOrigins[0] = $uri else . end' docker/keycloak/realm.json > /tmp/global-sales-demo-realm.json
-	mv /tmp/global-sales-demo-realm.json docker/keycloak/realm.json
-
-	export KEYCLOAK_CLIENT_SECRET=some-super-secret-key
-	jq --arg secret "${KEYCLOAK_CLIENT_SECRET}" '.clients[] |= if .clientId == "global-sales-demo" then .secret = $secret else . end' docker/keycloak/realm.json > /tmp/global-sales-demo-realm.json
-	mv /tmp/global-sales-demo-realm.json docker/keycloak/realm.json
-
-	jq --arg uri "https://${KEYCLOAK_BASE_URL}" '.clients[] |= if .clientId == "global-sales-demo" then .adminUrl = $uri else . end' docker/keycloak/realm.json > /tmp/global-sales-demo-realm.json
-	mv /tmp/global-sales-demo-realm.json docker/keycloak/realm.json
 
 	brew install yq
-	bash -c "echo \"Opening new shell\""
-	yq -i '.keycloak.host = strenv(KEYCLOAK_BASE_URL)' helm/stg/values.yaml
+	bash -c "echo \"Opening new shell after installing yq\""
+	echo "re-writing env specific values"
+	export AUTH_REDIRECT_URL="https://${CODESPACE_NAME}-443.app.github.dev/work/login/oauth2/code/github"
+	export POST_LOGOUT_REDIRECT_URL="https://${CODESPACE_NAME}-443.app.github.dev/work/"
+	yq -i '.flowable.work.envVariables."spring.security.oauth2.client.registration.github.redirect-uri" = strenv(AUTH_REDIRECT_URL)' helm/stg/values.yaml
+	yq -i '.flowable.work.envVariables."flowable.security.oauth2.post-logout-redirect-url" = strenv(POST_LOGOUT_REDIRECT_URL)' helm/stg/values.yaml
 
-	## Build keycloak image
-	# docker build -t keycloak-global-sales:12.0.8 docker/keycloak
-	# docker tag keycloak-global-sales:12.0.8 localhost:5001/keycloak-global-sales:20.0.0
-	# docker push localhost:5001/keycloak-global-sales:20.0.0
 }
 
 # Reusable function for deployment
