8eee3cb3ee6bced993a071e84ce1a2c17509c7f9
abretz-mimacom <133235099+abretz-mimacom@users.noreply.github.com>
2026-04-13 21:28:17 +0000

    create-env update

--- create-env.sh
+++ create-env.sh
@@ -3,7 +3,7 @@
 
 DISABLE_ARC="${4:-false}"
 
-/bin/bash -c "echo"
+/bin/bash -c "echo \"Updating values for this environment\""
 yq -i '.flowable.work.envVariables."spring.security.oauth2.client.registration.github.redirect-uri" = strenv(AUTH_REDIRECT_URL)' helm/stg/values.yaml
 yq -i '.flowable.work.envVariables."flowable.security.oauth2.post-logout-redirect-url" = strenv(POST_LOGOUT_REDIRECT_URL)' helm/stg/values.yaml
 
